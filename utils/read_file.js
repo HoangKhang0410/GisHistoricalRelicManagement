de7b3426a7d54c899e3eff5f0222dec9a9db030c
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-06-14 02:24:47 +0700

    api add cylinder

--- utils/read_file.js
+++ utils/read_file.js
@@ -5,59 +5,59 @@ const directoryPath = '../data/foundation/doors/center_entry';
 
 
 function readGeoJsonContent(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          reject(err)
-          return;
-        }
-      
-        var parentPath = "ngomon/"
-        var path = filePath.split("\\")
-        path.splice(0, 2)
-        path = parentPath + path.join("/")
-        console.log(path)
-      
-        const json = JSON.parse(data);
-        var resultData = []
-        if(json.features[0].geometry.type == "Polygon") {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err)
+        return;
+      }
 
-          json.features[0].geometry.coordinates.forEach((nodes) => {
-            var data = {}
-            data.name = json.features[0].properties["Building name"]
-            data.color = json.features[0].properties.color
-            data.width = json.features[0].properties.width
-            data.height = json.features[0].properties.height
-            data.path = path
-            data.nodes = nodes.map(node => ({
-                ["x"]: node[0],
-                ["y"]: node[1],
-                ["z"]: node[2]
-            }))
-            resultData.push(data)
-          });
-        } else {
-          var faces = json.features[0].geometry.coordinates
+      var parentPath = "ngomon/"
+      var path = filePath.split("\\")
+      path.splice(0, 2)
+      path = parentPath + path.join("/")
+      console.log(path)
+
+      const json = JSON.parse(data);
+      var resultData = []
+      if (json.features[0].geometry.type == "Polygon") {
+
+        json.features[0].geometry.coordinates.forEach((nodes) => {
           var data = {}
           data.name = json.features[0].properties["Building name"]
           data.color = json.features[0].properties.color
           data.width = json.features[0].properties.width
           data.height = json.features[0].properties.height
           data.path = path
-          data.faces = faces.map(face =>  face[0].map(node => ({
-              ["x"]: node[0],
-              ["y"]: node[1],
-              ["z"]: node[2]
-          })))
+          data.nodes = nodes.map(node => ({
+            ["x"]: node[0],
+            ["y"]: node[1],
+            ["z"]: node[2]
+          }))
           resultData.push(data)
-        }
-       
-        resolve(resultData)
-      });
-    })
+        });
+      } else {
+        var faces = json.features[0].geometry.coordinates
+        var data = {}
+        data.name = json.features[0].properties["Building name"]
+        data.color = json.features[0].properties.color
+        data.width = json.features[0].properties.width
+        data.height = json.features[0].properties.height
+        data.path = path
+        data.faces = faces.map(face => face[0].map(node => ({
+          ["x"]: node[0],
+          ["y"]: node[1],
+          ["z"]: node[2]
+        })))
+        resultData.push(data)
+      }
+
+      resolve(resultData)
+    });
+  })
 }
 
-module.exports = {readGeoJsonContent}
+module.exports = { readGeoJsonContent }
 
 
--- utils/read_json_path.js
+++ utils/read_json_path.js
@@ -3,19 +3,19 @@ const path = require('path');
 
 
 function getAllJsonFilePathInFolder(folderPath) {
-    return new Promise((resolve, reject) => {
-        fs.readdir(folderPath, (err, files) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-    
-          const jsonFiles = files.filter(file => path.extname(file).toLowerCase() === '.json');
-          const filePaths = jsonFiles.map(file => path.join(folderPath, file));
-    
-          resolve(filePaths);
-        });
-      });
+  return new Promise((resolve, reject) => {
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const jsonFiles = files.filter(file => path.extname(file).toLowerCase() === '.json');
+      const filePaths = jsonFiles.map(file => path.join(folderPath, file));
+
+      resolve(filePaths);
+    });
+  });
 }
 
-module.exports = {getAllJsonFilePathInFolder}
+module.exports = { getAllJsonFilePathInFolder }
