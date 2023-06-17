5db19ee2f401b97719d9595874daaf743cf3f26a
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-06-17 22:32:18 +0700

    deploy to vercel 12

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 
 
-app.use(cors({origin: '*'}))
+app.use(cors())
 
 const connectDB = async () => {
   try {
@@ -38,6 +38,25 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use("/api/auth", authRouter)
 app.use("/api/prism", prismRouter)
