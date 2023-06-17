17907c85928c805af2f5b22d951ca91e83ec56ee
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-06-17 23:08:05 +0700

    deploy to vercel 13

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
@@ -38,25 +46,6 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use("/api/auth", authRouter)
 app.use("/api/prism", prismRouter)
